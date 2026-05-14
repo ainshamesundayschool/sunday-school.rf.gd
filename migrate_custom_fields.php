e84cf40169c23d0d943d08493452f2f70f67c5a5
Peter Fayez <peterfayez107@gmail.com>
2026-05-15 01:57:38 +0300

    feat: add migration script and ignore rule for custom fields schema update

---
+++ migrate_custom_fields.php
@@ -0,0 +1,4 @@
+<?php
+// Simple script to migrate custom_fields json structures
+require_once 'config.php';
+// wait, config.php is probably needed but let's just use raw mysql if possible or write a script we can run
